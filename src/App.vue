40bd4e3583f52071ccdba8b34810163cbc8a588c
cingfong <44254681+cingfong@users.noreply.github.com>
2023-01-06 17:19:04 +0800

    fix: form title input

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
-  <div id="app">
-    <div>
+  <div id="app" class="w-full d-flex justify-content-center">
+    <div
+      v-if="titleFormShow"
+      class="form-title-input-wrap d-flex justify-content-center align-items-center"
+    >
+      <b-card
+        class="p-0"
+        bg-variant="dark"
+        text-variant="white"
+        title="請輸入抬頭名稱"
+      >
+        <b-input v-model="formTitle" class="form-title-input my-3"></b-input>
+        <b-button class="mt-2" variant="primary" @click="titleFormShow = false">
+          確定
+        </b-button>
+      </b-card>
+    </div>
+    <div class="form-wrap">
       <b-form>
         <b-form-input
           v-for="inputItem in inputList"
           :key="inputItem.model"
+          class="mt-2"
           id="input-1"
           v-model="formData[inputItem.model]"
           type="text"
@@ -12,7 +29,9 @@
           :disabled="inputItem.disabled"
           required
         />
-        <b-button @click="onSubmit" variant="primary">下一項</b-button>
+        <b-button @click="onSubmit" variant="primary" class="mt-2"
+          >下一項</b-button
+        >
       </b-form>
       {{ formList }}
     </div>
@@ -21,9 +40,9 @@
 
 <script>
 export default {
-  // name: "App",
   data() {
     return {
+      titleFormShow: true,
       inputList: [
         { text: "輸入項目", model: "name" },
         { text: "輸入規格", model: "criterion" },
@@ -33,6 +52,7 @@ export default {
         { text: "輸入小計", model: "total", disabled: true },
         { text: "輸入備註", model: "remark" },
       ],
+      formTitle: "",
       formData: {
         name: "",
         criterion: "",
@@ -50,6 +70,7 @@ export default {
       return this.formData.price * this.formData.number;
     },
   },
+  mounted() {},
   methods: {
     onSubmit() {
       this.formList.push({ ...this.formData });
@@ -59,4 +80,19 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.form-title-input-wrap {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.6);
+}
+.card-body {
+  width: 350px;
+}
+.form-title-input {
+}
+.form-wrap {
+  width: 350px;
+}
+</style>
